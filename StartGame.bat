9da6594d21a851949fc38aae606822a472bb5b3e
chzaksh <chzaksh@gmail.com>
2026-07-09 12:50:44 +0300

    add bat file

---
+++ StartGame.bat
@@ -0,0 +1,13 @@
+@echo off
+title Family Codenames Server
+echo =========================================
+echo Starting Family Codenames Game...
+echo =========================================
+echo.
+echo Press CTRL+C in this window to stop the game.
+echo.
+
+:: מריץ את שרת הפיתוח
+call npm run dev
+
+pause
